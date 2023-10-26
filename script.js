c1c0137f8e1afc5958ddf0e0755ed67fc01d2078
werrons <bakytsanzirov13@gmail.com>
2023-10-26 21:38:45 +0600

    new

---
+++ script.js
@@ -0,0 +1,71 @@
+// let a = "hello"
+// let result = a.charAt(0).toUpperCase(0) + a.slice(1)
+//
+// let b = ' Ivan'
+// let result2 = (b.toLowerCase());
+// console.log(result + result2)
+
+
+// let a = 'lorem ipsum dolor'
+// let b = a
+// console.log(b)
+
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice(18)
+// console.log(b)
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.slice (17, 21)
+// let c = a. slice (0,17)
+// let d = a.slice(21,26)
+// console. log(b + ' ' +  c + ' ' + d)
+
+
+// let a = 'children'
+// let b = a.slice(0,7).toUpperCase() + a.slice(7)
+// console.log(b)
+
+
+// let a = 'boss'
+// let b = a.slice(0, 1) + a.slice(2) + a.slice(1,2)
+// console.log(b)
+
+
+// let a = 'sunday'
+// let b = a.indexOf('d')
+// console.log(b)
+
+
+// let b = 'hello';
+// let newstr = b.replace(b, 'hekso');
+// console.log(newstr);
+
+
+// let a = 'user'
+// let b = 'new '
+// let c = a.slice(0, 2)
+// console.log(b + c)
+
+
+// let a = 'monday'
+// let b = 'day'
+// let c = b.slice(0, 3) + b.slice(0, 3) + a.slice(0, 3)
+// console.log(c)
+
+
+// let a = 'HELLO'
+// let b = a.slice(0, 1).toLowerCase() + a.slice(1, 2).toUpperCase() + a.slice(2, 3).toLowerCase() + a.slice(3, 4).toUpperCase() + a.slice(4, 5).toLowerCase()
+// console.log(b)
+
+
+// let a = 5
+// let b = '400'
+// let c = b.slice(0,1) + a + b.slice(1, 2)
+// console.log(c)
+
+
+// let a = 'monkey'
+// let b = 'banana'
+// let c = a.slice(0, 1) + b.slice(1, 4) + a.slice(3, 4)
+// console.log(c)
